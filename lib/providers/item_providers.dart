d9e87b9b51bda3d07143ccacc975d3cc38822d8f
Naman2004singh <naman.2singh9@gmail.com>
2025-02-27 01:34:24 +0530

    call the event detail api and pass event id in it

--- lib/providers/item_providers.dart
+++ lib/providers/item_providers.dart
@@ -1,10 +1,12 @@
 import 'package:csi_app/services/functions/event_api.dart';
+import 'package:csi_app/services/functions/event_detail_api.dart';
 import 'package:csi_app/services/models/all_event.dart';
 import 'package:csi_app/services/models/event_details_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final apiServiceProvider = Provider<EventApi>((ref) => EventApi());
-// final eventDetailServiceProvider = Provider<EventDetails>((ref) =>);
+final eventDetailServiceProvider =
+    Provider<EventDetailApi>((ref) => EventDetailApi());
 
 // provider to fetch item
 final allEventProvider = FutureProvider<List<AllEvent>>((ref) async {
@@ -15,7 +17,14 @@ final allEventProvider = FutureProvider<List<AllEvent>>((ref) async {
 final selectedEventId = StateProvider<int?>((ref) => null);
 
 // for other api call and use of event id
-// final eventDetailProvider = FutureProvider
+final eventDetailProvider =
+    FutureProvider<EventDetails>((ref) async {
+  final eventId = ref.watch(selectedEventId);
+  final apiService = ref.read(eventDetailServiceProvider);
+
+  // assert(eventId.)
+  return apiService.fetchDetails(eventId);
+});
 
 //     FutureProvider.autoDispose<List<AllEvent>>((ref) async {
 //   final apiService = ref.read(apiServiceProvider);
